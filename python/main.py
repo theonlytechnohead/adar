1cebde0bc43d4360e418734f4f0caaa4da438841
Craig Anderson <cranderson0110@gmail.com>
2023-04-14 15:29:37 +1200

    Added IPv6 address

--- python/main.py
+++ python/main.py
@@ -1,7 +1,7 @@
 
 import simplenc
 from zeroconf import ServiceBrowser, ServiceInfo, ServiceListener, Zeroconf
-from socket import getfqdn, gethostname, gethostbyname, inet_aton
+import socket
 
 
 class AdarListener(ServiceListener):
@@ -14,27 +14,32 @@ class AdarListener(ServiceListener):
 
     def add_service(self, zc: Zeroconf, type_: str, name: str) -> None:
         info = zc.get_service_info(type_, name)
-        print(f"Service added: {name}\n{info}")
+        print(f"Service added: {name}")
 
 
 def zeroconf():
     zeroconf = Zeroconf()
-    type = "_adar._tcp.local."
-    properties = {"Description": "Network coding for automated distributed storage systems"}
+    service_type = "_adar._tcp.local."
+    properties = {
+        "Description": "Network coding for automated distributed storage systems",
+    }
     port = 6780
-    fqdn = getfqdn()
-    hostname = gethostname()
-    address =  gethostbyname(hostname)
+    fqdn = socket.getfqdn()
+    hostname = socket.gethostname()
+    ipv4 = socket.gethostbyname(hostname)
+    # https://stackoverflow.com/questions/52081008/get-most-recent-global-ipv6-address-of-interface
+    ipv6 = socket.getaddrinfo(hostname, None, socket.AF_INET6)[1][4][0]
+    print(f"{hostname}: {fqdn} ({ipv4}, {ipv6})")
     adar_info = ServiceInfo(
-        type,
-        name=f"{hostname}.{type}",
+        service_type,
+        name=f"{hostname}.{service_type}",
         port=port,
         properties=properties,
         server=fqdn,
-        addresses=[inet_aton(address)])
+        addresses=[ipv4, ipv6])
     zeroconf.register_service(adar_info, 60)
     listener = AdarListener()
-    browser = ServiceBrowser(zeroconf, type, listener)
+    browser = ServiceBrowser(zeroconf, service_type, listener)
     try:
         input("Press enter to exit...\n\n")
     finally:
