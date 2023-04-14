5e8c9d9d77d298fc009c6ec141586560f7e4fa9a
Craig Anderson <cranderson0110@gmail.com>
2023-04-14 16:03:22 +1200

    Fixed IPv4 on Ubuntu, now uses all IPv4 addresses

--- python/main.py
+++ python/main.py
@@ -1,5 +1,7 @@
 
 import simplenc
+import ipaddress
+import netifaces
 from zeroconf import ServiceBrowser, ServiceInfo, ServiceListener, Zeroconf
 import socket
 
@@ -16,6 +18,17 @@ class AdarListener(ServiceListener):
         info = zc.get_service_info(type_, name)
         print(f"Service added: {name}")
 
+# https://stackoverflow.com/questions/39988525/find-local-non-loopback-ip-address-in-python
+def get_local_non_loopback_ipv4_addresses():
+    for interface in netifaces.interfaces():
+        # Not all interfaces have an IPv4 address:
+        if netifaces.AF_INET in netifaces.ifaddresses(interface):
+            # Some interfaces have multiple IPv4 addresses:
+            for address_info in netifaces.ifaddresses(interface)[netifaces.AF_INET]:
+                address_object = ipaddress.IPv4Address(address_info["addr"])
+                if not address_object.is_loopback:
+                    yield address_info["addr"]
+
 
 def zeroconf():
     zeroconf = Zeroconf()
@@ -26,7 +39,7 @@ def zeroconf():
     port = 6780
     fqdn = socket.getfqdn()
     hostname = socket.gethostname()
-    ipv4 = socket.gethostbyname(hostname)
+    ipv4 = list(get_local_non_loopback_ipv4_addresses())
     # https://stackoverflow.com/questions/52081008/get-most-recent-global-ipv6-address-of-interface
     ipv6 = socket.getaddrinfo(hostname, None, socket.AF_INET6)[1][4][0]
     print(f"{hostname}: {fqdn} ({ipv4}, {ipv6})")
@@ -36,7 +49,7 @@ def zeroconf():
         port=port,
         properties=properties,
         server=fqdn,
-        addresses=[ipv4, ipv6])
+        addresses=[*ipv4, ipv6])
     zeroconf.register_service(adar_info, 60)
     listener = AdarListener()
     browser = ServiceBrowser(zeroconf, service_type, listener)
