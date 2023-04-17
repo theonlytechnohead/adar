563bc2bfe0c7df6e8bef42f8542d0cb9adb4bc29
Craig Anderson <cranderson0110@gmail.com>
2023-04-17 18:35:59 +1200

    Added some constants, code cleanup

--- python/client.py
+++ python/client.py
@@ -1,12 +1,16 @@
 import socket
 import sys
 from zeroconf import ServiceBrowser, ServiceInfo, ServiceListener, Zeroconf, IPVersion
+from main import SERVICE
+
+V4 = 0
+V6 = 1
 
 
-def hello(v6, v4, port):
-    HOST, PORT = v6[0] if 0 < len(v6) else v4[0], port
+def hello(address, mode: int, port: int):
+    HOST, PORT = address, port
     data = " ".join(sys.argv[1:])
-    with socket.socket(socket.AF_INET6 if 0 < len(v6) else socket.AF_INET, socket.SOCK_STREAM) as sock:
+    with socket.socket(socket.AF_INET6 if mode else socket.AF_INET, socket.SOCK_STREAM) as sock:
         sock.connect((HOST, PORT))
         sock.sendall(bytes(data + "\n", "utf-8"))
         received = str(sock.recv(1024), "utf-8")
@@ -15,21 +19,24 @@ def hello(v6, v4, port):
 
 
 class AdarListener(ServiceListener):
-
-    def update_service(self, zc: Zeroconf, type_: str, name: str) -> None:
-        print(f"Service updated: {name}")
-
-    def remove_service(self, zc: Zeroconf, type_: str, name: str) -> None:
-        print(f"Service removed: {name}")
-
     def add_service(self, zc: Zeroconf, type_: str, name: str) -> None:
         info = zc.get_service_info(type_, name)
-        hello(info.parsed_addresses(IPVersion.V6Only), info.parsed_addresses(IPVersion.V4Only), info.port)
+        v6 = info.parsed_addresses(IPVersion.V6Only)
+        v4 = info.parsed_addresses(IPVersion.V4Only)
+        mode = V6 if 0 < len(v6) else V4
+        address = v6[0] if V6 else v4[0]
+        hello(address, mode, info.port)
+
+    def update_service(self, zc: Zeroconf, type_: str, name: str) -> None:
+        pass
+
+    def remove_service(self, zc: Zeroconf, type_: str, name: str) -> None:
+        pass
 
 
 zeroconf = Zeroconf()
 listener = AdarListener()
-browser = ServiceBrowser(zeroconf, "_adar._tcp.local.", listener)
+browser = ServiceBrowser(zeroconf, SERVICE, listener)
 
 try:
 	input("Press enter to exit...\n\n")
--- python/main.py
+++ python/main.py
@@ -1,4 +1,3 @@
-
 import simplenc
 import socketserver
 import ipaddress
@@ -8,26 +7,27 @@ from zeroconf import ServiceBrowser, ServiceInfo, ServiceListener, Zeroconf
 import socket
 import uuid
 
+ID = uuid.getnode()
+PORT = 6780
+SERVICE = "_adar._tcp.local."
+
 
 class AdarListener(ServiceListener):
-
-    def update_service(self, zc: Zeroconf, type_: str, name: str) -> None:
-        print(f"Service updated: {name}")
-
-    def remove_service(self, zc: Zeroconf, type_: str, name: str) -> None:
-        print(f"Service removed: {name}")
-
     def add_service(self, zc: Zeroconf, type_: str, name: str) -> None:
         info = zc.get_service_info(type_, name)
         print(f"Service added: {name}")
+
+    def update_service(self, zc: Zeroconf, type_: str, name: str) -> None:
+        pass
+
+    def remove_service(self, zc: Zeroconf, type_: str, name: str) -> None:
+        pass
 
 
 def get_local_non_loopback_ipv4_addresses():
     # https://stackoverflow.com/questions/39988525/find-local-non-loopback-ip-address-in-python
     for interface in netifaces.interfaces():
-        # Not all interfaces have an IPv4 address:
         if netifaces.AF_INET in netifaces.ifaddresses(interface):
-            # Some interfaces have multiple IPv4 addresses:
             for address_info in netifaces.ifaddresses(interface)[netifaces.AF_INET]:
                 address_object = ipaddress.IPv4Address(address_info["addr"])
                 if not address_object.is_loopback:
@@ -36,36 +36,33 @@ def get_local_non_loopback_ipv4_addresses():
 
 def zeroconf() -> Zeroconf:
     zeroconf = Zeroconf()
-    service_type = "_adar._tcp.local."
-    id = uuid.getnode()
     properties = {
         "Description": "Network coding for automated distributed storage systems",
-        "uuid": id
+        "uuid": ID
     }
-    port = 6780
     fqdn = socket.getfqdn()
     hostname = socket.gethostname()
     ipv4 = list(get_local_non_loopback_ipv4_addresses())
     # https://stackoverflow.com/questions/52081008/get-most-recent-global-ipv6-address-of-interface
     ipv6 = socket.getaddrinfo(hostname, None, socket.AF_INET6)[1][4][0]
-    print(f"{hostname} ({id}): {fqdn} ({ipv4}, {ipv6})")
+    print(f"{hostname} ({ID}): {fqdn} ({ipv4}, {ipv6})")
     adar_info = ServiceInfo(
-        service_type,
-        name=f"{hostname}.{service_type}",
-        port=port,
+        SERVICE,
+        name=f"{hostname}.{SERVICE}",
+        port=PORT,
         properties=properties,
         server=fqdn,
         addresses=[*ipv4, ipv6])
     zeroconf.register_service(adar_info, 60)
     listener = AdarListener()
-    browser = ServiceBrowser(zeroconf, service_type, listener)
+    browser = ServiceBrowser(zeroconf, SERVICE, listener)
     return zeroconf
 
 
 class AdarHandler(socketserver.StreamRequestHandler):
     def handle(self) -> None:
         self.data = self.rfile.readline().strip()
-        print(f"{self.client_address[0]} wrote: {str(self.data, 'utf-8')}")
+        print(f"{self.client_address[0]}: {str(self.data, 'utf-8')}")
         self.wfile.write(self.data.upper())
 
 
@@ -75,17 +72,11 @@ class dual_stack(socketserver.TCPServer):
             self.server_address, family=socket.AF_INET6, dualstack_ipv6=True)
 
 
-def server() -> socketserver.TCPServer:
-    address = ("::", 6780)
-    server = dual_stack(address, AdarHandler)
-    return server
-
-
 if __name__ == "__main__":
     service = zeroconf()
-    adar = server()
-    thread = threading.Thread(target=adar.serve_forever, daemon=True)
-    thread.start()
+    adar = dual_stack(("::", PORT), AdarHandler)
+    server = threading.Thread(target=adar.serve_forever, daemon=True)
+    server.start()
     try:
         input("Press enter to exit...\n\n")
     finally:
