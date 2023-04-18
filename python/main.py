cd670270b0236d17e31b42fc7c7e0b9e906fe4a7
Craig Anderson <cranderson0110@gmail.com>
2023-04-18 16:46:10 +1200

    More robust method of selecting IPv6 addresses
    Both for broadcast, and for connection

--- python/main.py
+++ python/main.py
@@ -15,7 +15,7 @@ ID = uuid.getnode()
 PORT = 6780
 SERVICE = "_adar._tcp.local."
 
-exit = False
+stop = False
 
 
 class AdarListener(ServiceListener):
@@ -43,6 +43,15 @@ def get_local_non_loopback_ipv4_addresses():
                     yield address_info["addr"]
 
 
+def get_local_non_loopback_ipv6_addresses():
+    for interface in netifaces.interfaces():
+        if netifaces.AF_INET6 in netifaces.ifaddresses(interface):
+            for address_info in netifaces.ifaddresses(interface)[netifaces.AF_INET6]:
+                address_object = ipaddress.IPv6Address(address_info["addr"])
+                if not address_object.is_loopback:
+                    yield address_info["addr"]
+
+
 def zeroconf() -> Zeroconf:
     zeroconf = Zeroconf()
     properties = {
@@ -52,8 +61,7 @@ def zeroconf() -> Zeroconf:
     fqdn = socket.getfqdn()
     hostname = socket.gethostname()
     ipv4 = list(get_local_non_loopback_ipv4_addresses())
-    # https://stackoverflow.com/questions/52081008/get-most-recent-global-ipv6-address-of-interface
-    ipv6 = socket.getaddrinfo(hostname, None, socket.AF_INET6)[1][4][0]
+    ipv6 = list(get_local_non_loopback_ipv6_addresses())
     print(f"{hostname} ({ID}): {fqdn} ({ipv4}, {ipv6})")
     adar_info = ServiceInfo(
         SERVICE,
@@ -61,7 +69,7 @@ def zeroconf() -> Zeroconf:
         port=PORT,
         properties=properties,
         server=fqdn,
-        addresses=[*ipv4, ipv6])
+        addresses=[*ipv4, *ipv6])
     zeroconf.register_service(adar_info)
     ServiceBrowser(zeroconf, SERVICE, AdarListener())
     return zeroconf
@@ -84,11 +92,15 @@ class dual_stack(socketserver.TCPServer):
             self.server_address, family=socket.AF_INET6, dualstack_ipv6=True)
 
 
-def parse_service(info: ServiceInfo) -> tuple[str, socket.AddressFamily]:
+def check_service(info: ServiceInfo) -> tuple[str, socket.AddressFamily]:
     v6 = info.parsed_addresses(IPVersion.V6Only)
-    if 0 < len(v6):
-        return v6[0], socket.AF_INET6
-    return info.parsed_addresses(IPVersion.V4Only)[0], socket.AF_INET
+    v4 = info.parsed_addresses(IPVersion.V4Only)
+    for address in v6:
+        if socket.getaddrinfo(address, PORT, socket.AF_INET6):
+            return address, socket.AF_INET6
+    for address in v4:
+        if socket.getaddrinfo(address, PORT, socket.AF_INET):
+            return v4[0], socket.AF_INET
 
 
 def check_pair(info: ServiceInfo) -> bool:
@@ -103,7 +115,7 @@ def check_pair(info: ServiceInfo) -> bool:
 
 def request_pair(info: ServiceInfo) -> bool:
     accepted = False
-    address, mode = parse_service(info)
+    address, mode = check_service(info)
     with socket.socket(mode, socket.SOCK_STREAM) as sock:
         sock.connect((address, PORT))
         sock.sendall(bytes("pair?" + "\n", "utf-8"))
@@ -131,11 +143,11 @@ def pair(name: str, info: ServiceInfo):
 
 
 def handle(signum, frame):
-    global exit
-    print("Exiting...")
+    global stop
+    print("Stopping...", end="")
     service.close()
     adar.shutdown()
-    exit = True
+    stop = True
 
 
 if __name__ == "__main__":
@@ -145,5 +157,6 @@ if __name__ == "__main__":
     adar = dual_stack(("::", PORT), AdarHandler)
     server = threading.Thread(target=adar.serve_forever)
     server.start()
-    while not exit:
+    while not stop:
         sleep(1)
+    print(" done")
