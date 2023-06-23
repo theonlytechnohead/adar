5eb96d03d480151542a0229d16a2889a461565a5
Craig Anderson <cranderson0110@gmail.com>
2023-06-23 16:38:28 +1200

    Proper naming conventions for this project

--- python/storage_fuse.py
+++ python/storage_fuse.py
@@ -18,45 +18,45 @@ class Storage(Operations):
     # Helpers
     # =======
 
-    def _full_path(self, partial):
-        if partial.startswith("/"):
-            partial = partial[1:]
-        path = os.path.join(self.root, partial)
+    def _root_path(self, path):
+        if path.startswith("/"):
+            path = path[1:]
+        path = os.path.join(self.root, path)
         return path
 
     # Filesystem methods
     # ==================
 
     def access(self, path, mode):
-        full_path = self._full_path(path)
-        if not os.access(full_path, mode):
+        root_path = self._root_path(path)
+        if not os.access(root_path, mode):
             raise FuseOSError(errno.EACCES)
 
     def chmod(self, path, mode):
-        full_path = self._full_path(path)
-        return os.chmod(full_path, mode)
+        root_path = self._root_path(path)
+        return os.chmod(root_path, mode)
 
     def chown(self, path, uid, gid):
-        full_path = self._full_path(path)
-        return os.chown(full_path, uid, gid)
+        root_path = self._root_path(path)
+        return os.chown(root_path, uid, gid)
 
     def getattr(self, path, fh=None):
-        full_path = self._full_path(path)
-        st = os.lstat(full_path)
+        root_path = self._root_path(path)
+        st = os.lstat(root_path)
         return dict((key, getattr(st, key)) for key in ('st_atime', 'st_ctime',
                      'st_gid', 'st_mode', 'st_mtime', 'st_nlink', 'st_size', 'st_uid'))
 
     def readdir(self, path, fh):
-        full_path = self._full_path(path)
+        root_path = self._root_path(path)
 
         dirents = ['.', '..']
-        if os.path.isdir(full_path):
-            dirents.extend(os.listdir(full_path))
+        if os.path.isdir(root_path):
+            dirents.extend(os.listdir(root_path))
         for r in dirents:
             yield r
 
     def readlink(self, path):
-        pathname = os.readlink(self._full_path(path))
+        pathname = os.readlink(self._root_path(path))
         if pathname.startswith("/"):
             # Path name is absolute, sanitize it.
             return os.path.relpath(pathname, self.root)
@@ -64,48 +64,48 @@ class Storage(Operations):
             return pathname
 
     def mknod(self, path, mode, dev):
-        return os.mknod(self._full_path(path), mode, dev)
+        return os.mknod(self._root_path(path), mode, dev)
 
     def rmdir(self, path):
-        full_path = self._full_path(path)
-        return os.rmdir(full_path)
+        root_path = self._root_path(path)
+        return os.rmdir(root_path)
 
     def mkdir(self, path, mode):
-        storage_backing.create(self._full_path(path), True)
-        return os.mkdir(self._full_path(path), mode)
+        storage_backing.create(self._root_path(path), True)
+        return os.mkdir(self._root_path(path), mode)
 
     def statfs(self, path):
-        full_path = self._full_path(path)
-        stv = os.statvfs(full_path)
+        root_path = self._root_path(path)
+        stv = os.statvfs(root_path)
         return dict((key, getattr(stv, key)) for key in ('f_bavail', 'f_bfree',
             'f_blocks', 'f_bsize', 'f_favail', 'f_ffree', 'f_files', 'f_flag',
             'f_frsize', 'f_namemax'))
 
     def unlink(self, path):
-        return os.unlink(self._full_path(path))
+        return os.unlink(self._root_path(path))
 
     def symlink(self, name, target):
-        return os.symlink(name, self._full_path(target))
+        return os.symlink(name, self._root_path(target))
 
     def rename(self, old, new):
-        return os.rename(self._full_path(old), self._full_path(new))
+        return os.rename(self._root_path(old), self._root_path(new))
 
     def link(self, target, name):
-        return os.link(self._full_path(target), self._full_path(name))
+        return os.link(self._root_path(target), self._root_path(name))
 
     def utimens(self, path, times=None):
-        return os.utime(self._full_path(path), times)
+        return os.utime(self._root_path(path), times)
 
     # File methods
     # ============
 
     def open(self, path, flags):
-        full_path = self._full_path(path)
-        return os.open(full_path, flags)
+        root_path = self._root_path(path)
+        return os.open(root_path, flags)
 
     def create(self, path, mode, fi=None):
-        full_path = self._full_path(path)
-        return os.open(full_path, os.O_WRONLY | os.O_CREAT, mode)
+        root_path = self._root_path(path)
+        return os.open(root_path, os.O_WRONLY | os.O_CREAT, mode)
 
     def read(self, path, length, offset, fh):
         os.lseek(fh, offset, os.SEEK_SET)
@@ -116,8 +116,8 @@ class Storage(Operations):
         return os.write(fh, buf)
 
     def truncate(self, path, length, fh=None):
-        full_path = self._full_path(path)
-        with open(full_path, 'r+') as f:
+        root_path = self._root_path(path)
+        with open(root_path, 'r+') as f:
             f.truncate(length)
 
     def flush(self, path, fh):
