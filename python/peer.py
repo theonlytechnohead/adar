3a523e703f83c54e5c433a5de4d61b102f88eea1
Craig Anderson <cranderson0110@gmail.com>
2023-09-21 13:24:15 +1200

    Peers can now be hashed (securely)
    Using SHA-3 256-bit

--- python/peer.py
+++ python/peer.py
@@ -1,3 +1,4 @@
+import hashlib
 import socket
 from dataclasses import dataclass
 
@@ -21,5 +22,11 @@ class Peer:
     data_address: tuple() = None
     data_connection: socket.socket = None
 
+    def secure_hash(self) -> bytes:
+        hashable_contents = []
+        hashable_contents.append(self.friendly_name)
+        hashable_contents.append(str(self.uuid))
+        hashable_contents.extend([str(version) for version in self.versions])
+        return hashlib.sha3_256("".join(hashable_contents).encode()).digest()
 
 peer_list: list[Peer] = []
