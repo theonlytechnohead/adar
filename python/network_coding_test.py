dc34f49471c61c8e23444f099fc75fbb118a7483
Craig Anderson <cranderson0110@gmail.com>
2023-10-04 14:26:37 +1300

    Further network coding efficiency testing
    I think I understand enough now to actually choose the right parameters and use it appropriately! Efficiency is now ~120% (for both number of packets and data size (including coefficients). Yet to be implemented... it seems as though I should extend the symbol size to the packet size for best results, which makes sense from a packet-wise view of the network as well.

--- python/network_coding_test.py
+++ python/network_coding_test.py
@@ -2,9 +2,8 @@ import random
 from simplenc import BinaryCoder
 
 
-def main(n = 8):
-    packets = n
-    packet_size = 8
+def main(packets: int = 1, length: int = 8):
+    packet_size = length
     seed = 1
     random.seed(seed)
 
@@ -12,6 +11,7 @@ def main(n = 8):
     decoder = BinaryCoder(packets, packet_size, seed)
 
     messages = 0
+    coded_size = 0
     while not decoder.is_fully_decoded():
         # print(f"Processing message {messages}")
         if messages < packets:
@@ -22,16 +22,18 @@ def main(n = 8):
             encoder.consume_packet(coefficients, packet)
         coefficient, packet = encoder.get_new_coded_packet()
         # https://stackoverflow.com/questions/32284940/python-bit-list-to-byte-list
-        print(int("".join(map(str, coefficient)), 2), int("".join(map(str, packet)), 2))
+        # print(int("".join(map(str, coefficient)), 2), int("".join(map(str, packet)), 2))
+        coded_size += len(coefficient) + len(packet)
         decoder.consume_packet(coefficient, packet)
         messages += 1
     
     if decoder.packet_vector == encoder.packet_vector:
-        efficiency = packets / messages
+        efficiency = messages / packets
         print(f"took {messages} messages to transmit {packets} packets ({efficiency * 100:.0f}%), {messages - packets} redundant")
+        print(f"coded size: {coded_size}, data size: {packets * packet_size} ({(coded_size / (packets * packet_size)) * 100:.0f}%)")
         # for packet in decoder.packet_vector:
             # print(packet)
-        return efficiency
+        return efficiency, (coded_size / (packets * packet_size)) * 100
     else:
         print(f"whoops, decoded packets vectors are wrong")
         print(f"encoded:\n", encoder.packet_vector)
@@ -39,9 +41,14 @@ def main(n = 8):
         raise ValueError
 
 if __name__ == "__main__":
-    main(10)
-    # efficiencies = []
-    # for n in range(10, 500, 20):
-        # efficiencies.append(main(n))
-    # average = sum(efficiencies) / len(efficiencies)
-    # print(f"average efficiency: {average * 100:.0f}%")
+    # main(2, 1500)
+    efficiencies = []
+    data_efficiencies = []
+    for n in range(1, 64, 8):
+        efficiency, data_efficiency = main(n, 1500)
+        efficiencies.append(efficiency)
+        data_efficiencies.append(data_efficiency)
+    average = sum(efficiencies) / len(efficiencies)
+    data_average = sum(data_efficiencies) / len(data_efficiencies)
+    print(f"average efficiency: {average * 100:.0f}%")
+    print(f"average data efficiency: {data_average:.0f}%")
